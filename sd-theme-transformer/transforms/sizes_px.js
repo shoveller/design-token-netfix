70f63714d8f66b770bdf503e261f2c8757344344
cinos81 <cinos81@gmail.com>
2023-02-07 14:16:54 +0900

    feat: 사이즈 트랜스포머의 버그를 수정

--- sd-theme-transformer/transforms/sizes_px.js
+++ sd-theme-transformer/transforms/sizes_px.js
@@ -5,10 +5,9 @@ module.exports.sizes_px = {
     name: 'sizes/px',
     type: 'value',
     matcher(token) {
-        const props = 'fontSize spacing borderRadius borderWidth sizing';
-        return props.split(' ').includes(token.type);
+        return ['fontSize', 'spacing', 'borderRadius', 'borderWidth', 'sizing'].includes(token.type);
     },
-    transformer(prop) {
-        return `${parseFloat(prop.original.value, 10)}px`;
+    transformer(token) {
+        return `${parseFloat(token.value)}px`;
     },
 }
