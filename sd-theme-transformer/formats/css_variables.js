fe7fbbe6ec9d3fe9945bdc7fcb854cfb96c635c4
cinos81 <cinos81@gmail.com>
2023-02-08 00:32:40 +0900

    fix: typo

--- sd-theme-transformer/formats/css_variables.js
+++ sd-theme-transformer/formats/css_variables.js
@@ -3,7 +3,8 @@
  */
 module.exports.css_variables = {
     name: 'css/variables',
-    formatter(dictionary ) {
+    formatter({dictionary} ) {
+        console.log(this.selector)
         return `${this.selector} {${dictionary.allProperties
             .map(({name, value}) => `  --${name}: ${value};`)
             .join('\n')}}`;
